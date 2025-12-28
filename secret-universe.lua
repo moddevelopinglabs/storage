02bee1c3bd62b4a39221a731acec7f320d4f87f3
AGreatNameIK <123846437+AGreatNameIK@users.noreply.github.com>
2025-12-28 17:31:57 -0500

    Add files via upload

---
+++ secret-universe.lua
@@ -0,0 +1,291 @@
+local Players = game:GetService("Players")
+
+local UILib = require("workspace/x11-colorpicker.lua")
+local hasWTS = type(WorldToScreen) == "function"
+
+print("Loading Secret Universe script!")
+print("Make sure you have Allow Unsafe LuaU Execution in Options!")
+
+local function getPing(raw)
+	local pingAddress = game:FindFirstChild("Stats"):FindFirstChild("PerformanceStats"):FindFirstChild("Ping").Address
+	local ping = memory_read("double", pingAddress + 0xC8)
+
+	if raw then
+		return ping
+	end
+
+	return ("Ping: %sms"):format(math.floor(ping))
+end
+
+local mainGui = UILib.new("Secret Universe", Vector2.new(320, 380), {getPing})
+
+local player = Players.LocalPlayer
+
+local visualsTab = mainGui:Tab("Visuals")
+local automationTab = mainGui:Tab("Automation")
+local otherTab = mainGui:Tab("Other")
+local espSection = mainGui:Section(visualsTab, "General")
+local farmSection = mainGui:Section(automationTab, "Auto Farm")
+local teleportSection = mainGui:Section(automationTab, "Teleports")
+local secretSection = mainGui:Section(automationTab, "Secrets")
+local infiniSection = mainGui:Section(automationTab, "Infinitower")
+local unloadSection = mainGui:Section(otherTab, "Unload")
+local debugSection = mainGui:Section(otherTab, "Debug")
+mainGui:CreateSettingsTab()
+
+local coinsESPOn = false
+
+local autoFarm = false
+local autoinfini = false
+local obbyTeleportSpeed = 2
+
+local running = true
+
+local obbyStage = 0
+local autoFarmMethod = "Endless Obby"
+
+local lastTeleport = os.clock()
+
+mainGui:Checkbox(automationTab, farmSection, "Auto Farm", false, function(state)
+	if state and autoFarmMethod == "Zombie" then
+		player.Character.HumanoidRootPart.Position = Vector3.new(-267, 3, -127)
+		task.wait(0.1)
+	end
+	autoFarm = state
+end)
+
+mainGui:Choice(automationTab, farmSection, "Method", {"Endless Obby"}, function(values)
+	autoFarmMethod = table.concat(values, ', ')
+end, {"Endless Obby", "Random Money Maker", "Zombie"}, false)
+
+mainGui:Slider(automationTab, farmSection, 'Teleport Speed', 2, function(value)
+	obbyTeleportSpeed = value
+end, 0.1, 5, 0.1, ' seconds')
+
+mainGui:Choice(automationTab, teleportSection, "Teleport", {}, function(values)
+	local teleportValue = table.concat(values, ', ')
+
+	if teleportValue == "Evil Arena" then
+		player.Character.HumanoidRootPart.Position = Vector3.new(-267, 3, -127)
+	elseif teleportValue == "Endless Obby" then
+		player.Character.HumanoidRootPart.Position = Vector3.new(270, 3, 0)
+	elseif teleportValue == "Rebirth Door" then
+		player.Character.HumanoidRootPart.Position = Vector3.new(15, 4, -14.5)
+	elseif teleportValue == "Secret Meetup Outer" then
+		player.Character.HumanoidRootPart.Position = Vector3.new(12.5, 505, 0)
+	elseif teleportValue == "Secret Meetup Inner" then
+		player.Character.HumanoidRootPart.Position = Vector3.new(235, 235, 43)
+	end
+end, {"Evil Arena", "Endless Obby", "Secret Meetup Outer", "Secret Meetup Inner", "Rebirth Door"}, false)
+
+mainGui:Choice(automationTab, secretSection, "Collect Secret", {}, function(values)
+	local secretValue = table.concat(values, ', ')
+	local ogPosition = player.Character.HumanoidRootPart.Position
+
+	if secretValue == "Lorem Ipsum" then
+		player.Character.HumanoidRootPart.Position = Vector3.new(12.7, 3, -266)
+		task.wait(0.1)
+		player.Character.HumanoidRootPart.Position = Vector3.new(-39.5, 3, -365)
+		task.wait(0.1)
+		player.Character.HumanoidRootPart.Position = Vector3.new(12.7, 3, -414)
+		task.wait(0.1)
+		player.Character.HumanoidRootPart.Position = Vector3.new(12.8, 3, -322)
+		task.wait(0.1)
+		player.Character.HumanoidRootPart.Position = Vector3.new(-39.5, 3, -414)
+		task.wait(0.1)
+		player.Character.HumanoidRootPart.Position = Vector3.new(12.8, 3, -322)
+	elseif secretValue == "Post-mortem" then
+		player.Character.HumanoidRootPart.Position = Vector3.new(12.7, 3, -266)
+		task.wait(0.1)
+		player.Character.HumanoidRootPart.Position = Vector3.new(-1.3, 3.5, -413)
+		task.wait(0.1)
+		player.Character.HumanoidRootPart.Position = Vector3.new(12.8, 3, -322)
+	elseif secretValue == "Random Number Generation" then
+		local rainberge = workspace.Terrain:FindFirstChild("Rainberge")
+		if rainberge then
+			player.Character.HumanoidRootPart.Position = rainberge.Position
+		end
+	end
+	task.wait(0.1)
+	player.Character.HumanoidRootPart.Position = ogPosition
+end, {"Lorem Ipsum", "Post-mortem", "Random Number Generation"}, false)
+
+mainGui:Checkbox(automationTab, infiniSection, "Auto TP Buttons", false, function(state)
+	autoinfini = state
+end)
+
+mainGui:Checkbox(otherTab, unloadSection, "Unload", false, function(state)
+	running = false
+end)
+
+mainGui:Checkbox(otherTab, debugSection, "Grab Player Position", false, function(state)
+	print("Current player position is", player.Character.HumanoidRootPart.Position)
+end)
+
+local function resetValues()
+	obbyStage = 0
+end
+
+local function WorldToScreen_Safe(pos)
+	if not hasWTS then
+		return
+	end
+	local ok, res = pcall(function()
+		return WorldToScreen(pos)
+	end)
+	if ok then
+		return res
+	end
+	return
+end
+
+local function indexCoins()
+	local coins = {}
+	for _, part in workspace:GetDescendants() do
+		if not part:IsA("BasePart") then
+			continue
+		end
+		local name = part.Name
+		local springSplit = string.split(name, "FloatingCoin")
+
+		if springSplit[1] ~= "" then
+			continue
+		end
+
+		local SUCoin = part:FindFirstChild("SUCoin")
+
+		if SUCoin then
+			coins[part] = part
+		end
+	end
+	return coins
+end
+
+local activeSquares = {} -- [Drawing] = expireTime
+local cachedCoins = indexCoins()
+
+mainGui:Checkbox(visualsTab, espSection, "Coins", false, function(state)
+	cachedCoins = indexCoins()
+	coinsESPOn = state
+end)
+
+local function cacheInvalid()
+	for coin in pairs(cachedCoins) do
+		if not coin:IsDescendantOf(workspace) then
+			return true
+		end
+		if not coin:FindFirstChild("SUCoin") then
+			return true
+		end
+	end
+	return false
+end
+
+local function drawCoin(coin)
+	local drawingPosition = WorldToScreen_Safe(coin.Position)
+	if not drawingPosition then
+		return
+	end
+
+	local square = Drawing.new("Square")
+	square.Filled = true
+	square.Color = Color3.fromRGB(255, 0, 0)
+	square.Position = drawingPosition
+	square.Size = Vector2.new(10, 10)
+	square.Visible = true
+	square.Corner = 20
+
+	activeSquares[square] = os.clock() + 0.05
+end
+
+print("Loaded!")
+
+while running do
+	task.wait(0.0015)
+	mainGui:Step()
+	if autoFarm then
+		if autoFarmMethod == "Zombie" then
+			player.Character.HumanoidRootPart.Position = Vector3.new(-315, 50, -127)
+		elseif autoFarmMethod == "Random Money Maker" then
+			player.Character.HumanoidRootPart.Position = Vector3.new(-154, 4, -233)
+		elseif autoFarmMethod == "Endless Obby" then
+			local currentTime = os.clock()
+
+			if currentTime - lastTeleport >= obbyTeleportSpeed then
+				local stage = workspace.Structures["Endless Obby"].Stages[obbyStage]
+
+				if stage then
+					local position = stage.Finish.Position
+					player.Character.HumanoidRootPart.Position = Vector3.new(position.X, position.Y + 2, position.Z)
+					obbyStage += 1
+				else
+					if obbyStage ~= 0 then
+						obbyStage -= 1
+					else
+						player.Character.HumanoidRootPart.Position = Vector3.new(270, 3, 0)
+					end
+				end
+				lastTeleport = currentTime
+			end
+		end
+	end
+	if autoinfini then
+		local currentTime = os.clock()
+
+		if currentTime - lastTeleport >= 2 then
+			for _, child in workspace.Structures.Infinitower.Floors:GetDescendants() do
+				if child.Parent.Name == "Buttons" then
+					for _, button in child.Parent:GetChildren() do
+						if button.Transparency == 0 then
+							local position = button.Position
+							if position then
+								player.Character.HumanoidRootPart.Position = Vector3.new(position.X, position.Y + 2, position.Z)
+							end
+						else
+							continue
+						end
+					end
+				else
+					continue
+				end
+			end
+			lastTeleport = currentTime
+		end
+	end
+	if coinsESPOn then
+		if cacheInvalid() then
+			cachedCoins = indexCoins()
+		end
+		-- draw coins
+		for _, coin in cachedCoins do
+			drawCoin(coin)
+		end
+
+		-- cleanup expired squares
+		local now = os.clock()
+		for square, expireTime in pairs(activeSquares) do
+			if now >= expireTime then
+				square:Remove()
+				activeSquares[square] = nil
+			end
+		end
+	else
+		for square, _ in pairs(activeSquares) do
+			square:Remove()
+			activeSquares[square] = nil
+		end
+	end
+	if player.Character then
+		local character = player.Character
+
+		local humanoid = character:FindFirstChild("Humanoid")
+		if humanoid and humanoid.Health <= 0 then
+			resetValues()
+		end
+	end
+end
+for square, _ in pairs(activeSquares) do
+	square:Remove()
+	activeSquares[square] = nil
+end
+mainGui:Destroy()
